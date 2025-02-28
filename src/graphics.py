c12ca02d260fc02a65318dc3ea95d28bca2219e1
tkdlrs <tkd.lsanchez@gmail.com>
2025-02-28 16:58:08 -0700

    Drawing some lines.

--- src/graphics.py
+++ src/graphics.py
@@ -5,10 +5,10 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False        
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -20,6 +20,24 @@ class Window:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+    
     def close(self):
         self.__running = False
+
+#
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_one, point_two):
+        self.p1 = point_one
+        self.p2 = point_two
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x,self.p1.y, self.p2.x,self.p2.y, fill=fill_color, width=2)
+
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    #
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, "red")
+    #
     win.wait_for_close()
 
 
