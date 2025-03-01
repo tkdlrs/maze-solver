0c42aeb553dec54c8b011ee80c84b8d97d258248
tkdlrs <tkd.lsanchez@gmail.com>
2025-03-01 12:30:43 -0700

    My attempt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
     #
-    line = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(line, "red")
+    # line = Line(Point(50, 50), Point(400, 400))
+    # win.draw_line(line, "red")
+    cell = Cell(Point(50, 50), Point(100, 100), win, has_top_wall=False)
+    cell = Cell(Point(50, 50), Point(100, 100), win, has_right_wall=False)
+    cell = Cell(Point(50, 50), Point(100, 100), win, has_bottom_wall=False)
+    cell = Cell(Point(50, 50), Point(100, 100), win, has_left_wall=False)
+    # cell2 = Cell(Point(0, 0), Point(-10, -10), win)
+    cell.draw()
+    # cell2.draw()
     #
     win.wait_for_close()
 
